015fe62488bb0f298b0f3abc8da01be2a616efae
Tiagosvi <102734829+Tiagosvi@users.noreply.github.com>
2025-06-10 12:42:01 -0300

    Create SaldosTests.java

---
+++ src/test/java/aed/SaldosTests.java
@@ -0,0 +1,53 @@
+package aed;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SaldosTests {
+
+ //   @Test
+   // void sinSaldos() {
+     //   Saldos s = new Saldos(0);
+
+       // assertEquals(0, s.maximoTenedor());
+    //}
+
+
+    @Test
+    void actualizarAumentaSaldo(){
+        Saldos s = new Saldos(5);
+        assertEquals(1, s.maximoTenedor());
+        s.actualizar(4, 17);
+        assertEquals(4, s.maximoTenedor());
+        s.actualizar(2, 25);
+        assertEquals(2, s.maximoTenedor());
+
+
+    }
+
+    @Test
+    void actualizarDisminuyeSaldo(){
+        Saldos s = new Saldos(5);
+        assertEquals(1, s.maximoTenedor());
+        s.actualizar(4, 17);
+        assertEquals(4, s.maximoTenedor());
+        s.actualizar(2, 15);
+        s.actualizar(4, 10);
+        assertEquals(2, s.maximoTenedor());
+        
+
+
+    }
+
+
+    @Test
+    void desempatePorId() {
+        Saldos s = new Saldos(5);
+        s.actualizar(2,10);
+        s.actualizar(5,10);
+
+        assertEquals(2,s.maximoTenedor());
+
+    }
+}
