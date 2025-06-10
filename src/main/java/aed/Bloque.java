90f7fdfc099a74120c940bc24af7e4d02074a4b7
fshiri7 <nicolas.sck15@gmail.com>
2025-06-10 04:16:27 -0300

    Expandi la clase bloque
    ultima update por la noche, la clase bloque tiene unos bugs que hay que arreglar, y algunos metodos restan por implementar, no es mucho el trabajo que queda, y las bases ya estan sentadas, es cuestion de ponerse a finalizar los ultimos fixes

--- src/main/java/aed/Bloque.java
+++ src/main/java/aed/Bloque.java
@@ -4,10 +4,18 @@ public class Bloque implements ListaPrioridad<Transaccion> {
     private Transaccion[] interno;
     private int tamaño;
     private int montoTotal;
-    private int[] handles; //N por orden de transaccion
+    private int[] internoT; //N por orden de transaccion
 
     public Bloque(Transaccion[] transacciones) {
-        interno = new Transaccion[transacciones.length];
+        interno = transacciones;
+        tamaño = transacciones.length;
+        if (transacciones.length != 0) {
+            int n = tamaño -1;
+            while (n >= 0) {
+                heapifyDown(n);
+                n -= 1;
+            }
+        }
     }
 
     public int montoTotal() {
@@ -22,18 +30,44 @@ public class Bloque implements ListaPrioridad<Transaccion> {
         return interno[0];
     }
 
-    public void agregarAdelante() {
-    }
-
-    public void agregarAtras() {
-    }
 
     public void eliminar() {
+        
     }
 
     private void actualizar() {
     }
 
-    private void heapify() {
-    }
+    private void heapifyDown(int n) {
+        int HijoIzq = (2*n)+1;
+        int HijoDer = (2*n)+2;
+        Transaccion TransaccionBaja = interno[n];
+
+        if(HijoIzq < tamaño) {
+            Transaccion HijoIT = interno[HijoIzq];
+            if (HijoDer < tamaño) {
+                Transaccion HijoDT = interno[HijoDer];
+                int c = interno[HijoIzq].compareTo(interno[HijoDer]);
+                if (c == 1) {
+                    int s = TransaccionBaja.compareTo(HijoIT);
+                    if (s == -1) {
+                        interno[n] = HijoIT;
+                        interno[HijoIzq] = TransaccionBaja;
+                    } else {}
+                } else if (c == -1) {
+                    int s = TransaccionBaja.compareTo(HijoDT);
+                    if (s == -1) {
+                        interno[n] = HijoDT;
+                        interno[HijoDer] = TransaccionBaja;
+                } else {}
+                } 
+            }else { 
+                int s = TransaccionBaja.compareTo(HijoIT);
+                if (s == -1) {
+                    interno[n] = HijoIT;
+                    interno[HijoIzq] = TransaccionBaja;
+                }
+            }
+        }
+    }   //N esto hay que implementar para que siga bajando todo lo que sea necesario (y seguramente se puede emprolijar significativamente)
 }
