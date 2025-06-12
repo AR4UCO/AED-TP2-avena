86709c37480cb46a4c7c3e1778f85bfb670a71d0
AR4UCO <122570020+AR4UCO@users.noreply.github.com>
2025-06-12 00:02:34 -0300

    Update berretaTests.java

--- src/test/java/aed/berretaTests.java
+++ src/test/java/aed/berretaTests.java
@@ -1,68 +1,61 @@
 package aed;
 
 import static org.junit.jupiter.api.Assertions.*;
-import java.util.ArrayList;
 import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
-import jdk.jfr.Timestamp;
 
 
-class BerretaCoinPropios {
-
-
-
-    //id,id_C,id_V,monto
+class BerretaCoinPropiosTest {
 
 
     @Test
-    public void caso1(){  //Agrego 2 bloques. Chequeo MAXTenedor/MayUltBloque
+    public void ultBloq(){  //Agrego 2 bloques. Chequeo MAXTenedor/MayUltBloque
         
         Berretacoin berretacoin = new Berretacoin(3);
 
         Transaccion[] bloque1 = {
-            new Transaccion(0,0,2,1), //C. el 2 tiene $1
-            new Transaccion(1,2,1,1) // 1 = $1
+            new Transaccion(0,0,2,1), 
+            new Transaccion(1,2,1,1) 
         };
         
         berretacoin.agregarBloque(bloque1);
-        assertEquals(2, berretacoin.maximoTenedor());
+        assertEquals(1, berretacoin.maximoTenedor());
         
         Transaccion[]  bloque2 = {
-            new Transaccion(0,0,2,2), //C. el 2 tiene $2
-            new Transaccion(1,1,3,1), // 3 = $1
-            new Transaccion(2,2,3,2)  // 3 = $2
-        };  //ACA el 1 deberia tener 0, el 2 deberia tener 0, y el 3 deberia tener 3
+            new Transaccion(0,0,2,2), 
+            new Transaccion(1,1,3,1), 
+            new Transaccion(2,2,3,2)  
+        };  
         
         berretacoin.agregarBloque(bloque2);
 
         assertEquals(3, berretacoin.maximoTenedor());
-        assertEquals(2, berretacoin.txMayorValorUltimoBloque());
-
+        assertEquals(berretacoin.txMayorValorUltimoBloque(), new Transaccion(2,2,3,2));
         berretacoin.hackearTx();
 
-        assertEquals(0, berretacoin.txMayorValorUltimoBloque());
-        assertTrue(Arrays.equals((Arrays.copyOfRange(bloque, 0, bloque.length - 1)), berretacoin.txUltimoBloque()));
+        assertEquals(berretacoin.txMayorValorUltimoBloque(), new Transaccion(0,0,2,2) );
+        assertTrue(Arrays.equals((Arrays.copyOfRange(bloque2, 0, bloque2.length - 1)), berretacoin.txUltimoBloque()));
 
     }
 
     @Test 
-    public void caso2(){ // 3 usuarios con mismo saldo
+    public void maximoTenedorIgSaldo(){ //3 uuarios con igual saldo
 
         Berretacoin bc = new Berretacoin(6);
         
         Transaccion[] bloque1 = {
-            new Transaccion(0,0,4,1), //C. 4=$1
-            new Transaccion(1,4,1,1) //  1 = $1, 4= 0  
+            new Transaccion(0,0,4,1), 
+            new Transaccion(1,4,1,1) 
         };
 
         bc.agregarBloque(bloque1);
         
         Transaccion[] bloque2 = {
-            new Transaccion(0,0,3,2), //C. 3= 2 , 1 = 1
-            new Transaccion(1,1,6,1), //  6=1
-            new Transaccion(2,3,6,2), //  6 = 3  
-            new Transaccion(3,6,5,3)  // 5 = 3
+            new Transaccion(0,0,3,2), 
+            new Transaccion(1,1,6,1), 
+            new Transaccion(2,3,6,2), 
+            new Transaccion(3,6,5,3)  
         };
 
         bc.agregarBloque(bloque2);
@@ -74,16 +67,16 @@ class BerretaCoinPropios {
 
 
         Transaccion[] bloque3 = {
-            new Transaccion(0,0,4,3), // 4 = 3
+            new Transaccion(0,0,4,3), 
         };
 
         bc.agregarBloque(bloque3);
 
         Transaccion[] bloque4 = {
-            new Transaccion(0,0,2,4), //C. 2=4
-            new Transaccion(1,2,3,3), //   3 = 3
-            new Transaccion(2,4,5,3), //    5  = 3 
-            new Transaccion(3,3,1,3) // 1 = 3
+            new Transaccion(0,0,2,4), 
+            new Transaccion(1,2,3,3), 
+            new Transaccion(2,4,5,3), 
+            new Transaccion(3,3,1,3) 
  
         };
       
@@ -92,25 +85,21 @@ class BerretaCoinPropios {
         // 1 = $3, 6 = $3, 5 = $3, 2 = $1
         
         assertEquals(1, bc.maximoTenedor());
-        bc.hackearTx();
 
-        // 3 = $3, 6 = $3, 5 = $3, 2 = $1+
-
-        assertEquals(3, bc.maximoTenedor());
         
         
 
     }
 
     @Test 
-    public void hackeo_Y_vacio(){
+    public void hackeo_Y_vacio(){ //hackearTX y queda vacio
         Berretacoin bc = new Berretacoin(3);
         
         Transaccion[] bloque1 = {
-            new Transaccion(0,0,2,1), // 2 = 1
-            new Transaccion(1,2,1,1), // 1 = 1
-            new Transaccion(2,1,3,1), // 3 = 1
-            new Transaccion(3,3,1,1) // 1 = 1        
+            new Transaccion(0,0,2,1), 
+            new Transaccion(1,2,1,1), 
+            new Transaccion(2,1,3,1), 
+            new Transaccion(3,3,1,1)        
         };
 
         bc.agregarBloque(bloque1);
@@ -119,7 +108,7 @@ class BerretaCoinPropios {
         bc.hackearTx();
         bc.hackearTx();
         assertEquals(1, bc.maximoTenedor());
-        assertTrue(Arrays.equals((Arrays.copyOfRange(bloque, 0, bloque.length - 4)), berretacoin.txUltimoBloque()));
+        assertTrue(Arrays.equals((Arrays.copyOfRange(bloque1, 0, bloque1.length - 4)), bc.txUltimoBloque()));
         
     }
 
@@ -129,17 +118,17 @@ class BerretaCoinPropios {
         Berretacoin bc = new Berretacoin(6);
         
         Transaccion[] bloque1 = {
-            new Transaccion(0,0,4,1), //C. 4=$1
-            new Transaccion(1,4,1,1) //  1 = $1, 4= 0  
+            new Transaccion(0,0,4,1), 
+            new Transaccion(1,4,1,1)  
         };
 
         bc.agregarBloque(bloque1);
         
         Transaccion[] bloque2 = {
-            new Transaccion(0,0,3,2), //C. 3= 2 , 1 = 1
-            new Transaccion(1,1,6,1), //  6=1
-            new Transaccion(2,3,6,2), //  6 = 3  
-            new Transaccion(3,6,5,3)  // 5 = 3
+            new Transaccion(0,0,3,2), 
+            new Transaccion(1,1,6,1), 
+            new Transaccion(2,3,6,2),  
+            new Transaccion(3,6,5,3)  
         };
 
         bc.agregarBloque(bloque2);
@@ -147,17 +136,17 @@ class BerretaCoinPropios {
 
 
         Transaccion[] bloque3 = {
-            new Transaccion(0,0,4,3), // 4 = 3
+            new Transaccion(0,0,4,3), 
         };
 
         bc.agregarBloque(bloque3);
         assertEquals(0, bc.montoMedioUltimoBloque());
 
         Transaccion[] bloque4 = {
-            new Transaccion(0,0,2,4), //C. 2=4
-            new Transaccion(1,2,3,3), //   3 = 3
-            new Transaccion(2,4,6,3), //    6  = 3 
-            new Transaccion(3,3,1,3), // 1 = 3
+            new Transaccion(0,0,2,4), 
+            new Transaccion(1,2,3,3), 
+            new Transaccion(2,4,6,3), 
+            new Transaccion(3,3,1,3), 
             new Transaccion(4,2,4,1)
         };
       
@@ -174,8 +163,3 @@ class BerretaCoinPropios {
 
 
 }
-
-
-
-
-
