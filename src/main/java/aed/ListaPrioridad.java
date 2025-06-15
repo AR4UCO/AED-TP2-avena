1f2fb379d437178adae53295a1589d37442760ce
Augusto-DC <dcaugusto03@gmail.com>
2025-06-15 19:50:37 -0300

    Update ListaPrioridad.java

--- src/main/java/aed/ListaPrioridad.java
+++ src/main/java/aed/ListaPrioridad.java
@@ -4,13 +4,8 @@ public interface ListaPrioridad<T> {
 
     public T mayorValor();
 
-    public void agregarAtras();
-
-    public void agregarAdelante();
-
     public int montoTotal();
 
     public int longitud();
 
-    public void eliminar();
 }
