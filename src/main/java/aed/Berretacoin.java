901446fdba56624dd165cfd679ab825c78dc851c
Augusto-DC <dcaugusto03@gmail.com>
2025-06-15 12:08:04 -0300

    Breve comentarios berretacoin

--- src/main/java/aed/Berretacoin.java
+++ src/main/java/aed/Berretacoin.java
@@ -1,16 +1,16 @@
 package aed;
 
-public class Berretacoin {
+public class Berretacoin {  
     private Saldos saldo;
     private BlockChain BlockChain;
 
 
-    public Berretacoin(int n_usuarios){
+    public Berretacoin(int n_usuarios){ //O(P)
         saldo = new Saldos(n_usuarios);
         BlockChain = new BlockChain();
     }
 
-    public void agregarBloque(Transaccion[] transacciones){
+    public void agregarBloque(Transaccion[] transacciones){ //O(n*log(p))
 
         for (Transaccion t : transacciones) {
             if (t.id_comprador() == 0) {
@@ -24,19 +24,19 @@ public class Berretacoin {
         BlockChain.agregarAtras(bloque);
     }
 
-    public Transaccion txMayorValorUltimoBloque(){
+    public Transaccion txMayorValorUltimoBloque(){ //O(1)
         return BlockChain.ultimo().mayorValor();
     }
 
-    public Transaccion[] txUltimoBloque(){
+    public Transaccion[] txUltimoBloque(){ //O(n)
         return BlockChain.ultimo().bloquexId();
     }
 
-    public int maximoTenedor(){
+    public int maximoTenedor(){ //O(1)
         return saldo.maximoTenedor();
     }
 
-    public int montoMedioUltimoBloque(){
+    public int montoMedioUltimoBloque(){ //O(1)
         Bloque ultimo = BlockChain.ultimo();
         if (ultimo.tieneCreacion() == true) {
             if (ultimo.longitud() == 1) {
@@ -49,7 +49,7 @@ public class Berretacoin {
         }
     }
 
-    public void hackearTx(){
+    public void hackearTx(){ //O(log(n) + log(p))
         Bloque ultimo = BlockChain.ultimo();
         Transaccion hackeada = ultimo.mayorValor();
 
