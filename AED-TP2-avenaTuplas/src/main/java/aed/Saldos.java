69061da92290e9967813fd4628350916ca7ae21a
Augusto-DC <dcaugusto03@gmail.com>
2025-07-07 19:44:31 -0300

    Update Saldos.java

--- AED-TP2-avenaTuplas/src/main/java/aed/Saldos.java
+++ AED-TP2-avenaTuplas/src/main/java/aed/Saldos.java
@@ -1,11 +1,12 @@
 package aed;
 
 public class Saldos {
+
     private ColaPrioridad<Usuario> colaDeUsuarios;
     private Usuario[] sal;
-    
 
     public class Usuario implements Comparable<Usuario> {
+
         private int id;
         private int balance;
 
@@ -14,43 +15,53 @@ public class Saldos {
             balance = 0;
         }
 
-        public int obtid(){
+        public int obtid() {
             return id;
         }
 
         @Override
         public int compareTo(Usuario n) {
             int v = 0;
-            if (this.balance > n.balance || (this.balance == n.balance && this.id < n.id)) {
-                v = 1; // N el original va arriba
+            if (this.balance > n.balance) {
+                return 1;
+            } else if (this.balance < n.balance) {
+                return -1;
             } else {
-                v = -1; // N el segundo va arriba
+                if (this.id < n.id) {
+                    return 1;
+                } else if (this.id > n.id) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+
             }
-            return v;
         }
     }
 
-
-
     public Saldos(int n) { // ver
-        sal = new Usuario[n];
+        sal = new Usuario[n + 1];
         int c = 1;
-        while (c <= n ) { // O(n)
-            sal[c-1] = new Usuario(c);
+        while (c <= n) { // O(n)
+            sal[c] = new Usuario(c);
             c += 1;
         }
-        colaDeUsuarios = new ColaPrioridad<Usuario>(sal);
-    }  
-
+        Usuario[] data = new Usuario[n];
+        int i = 0;
+        while (i < n) {
+            data[i] = sal[i + 1];
+            i++;
+        }
+        colaDeUsuarios = new ColaPrioridad<Usuario>(data);
+    }
 
     public int maximoTenedor() {
         return colaDeUsuarios.maximo().obtid();
     }
-    
-    public void actualizar(int handle, int subeObaja) { 
+
+    public void actualizar(int handle, int subeObaja) {
         sal[handle].balance += subeObaja;
-        colaDeUsuarios.actualizar(handle , subeObaja);
+        colaDeUsuarios.actualizar(handle - 1, subeObaja);
     }
 
-   
 }
