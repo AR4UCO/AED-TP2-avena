21592e7b6a6ff0391503cf61619c62491c186eb8
Augusto-DC <dcaugusto03@gmail.com>
2025-07-08 14:13:29 -0300

    Version Full

--- AED-TP2-avenaTuplas/src/main/java/aed/Bloque.java
+++ AED-TP2-avenaTuplas/src/main/java/aed/Bloque.java
@@ -1,6 +1,11 @@
 package aed;
 
+import javax.xml.namespace.QName;
+
+import sun.util.resources.cldr.ext.TimeZoneNames_zh_Hans_SG;
+
 public class Bloque {
+
     private ColaPrioridad<Transaccion> colaDetransacciones;
     private boolean creacion;
     private Transaccion[] interno;
@@ -40,26 +45,26 @@ public class Bloque {
 
     public Transaccion[] bloquexId() { //tengo que ver como acceder al segundo elemento de cada tupla
         Transaccion[] res = new Transaccion[this.longitud()];
-        int c = 0;
         if (this.longitud() == 0) {
             return res;
         }
+        int c = 0;
         int i = 0;
-        while (i<interno.length){
+        while (i < interno.length) {
             if (colaDetransacciones.obtenerPosicion(i) != -1) {
-                res[c] = interno[colaDetransacciones.obtenerPosicion(i)];
+                res[c] = interno[i]; //Veerifico que la posicion en i no sea -1 y accedo a ella.
                 c += 1;
             }
-            i +=1;
+            i += 1;
 
         }
         return res;
     }
 
     public void eliminarMaximo() {
-        //if (colaDetransacciones[0].primero.id_comprador() != 0) {
-         //   montoTotal -= colaDetransacciones[0].primero.monto();
-        //}
+        if (colaDetransacciones.maximo().id_comprador() != 0) {
+            montoTotal -= colaDetransacciones.maximo().monto();
+        }
         colaDetransacciones.desencolar();
     }
 }
